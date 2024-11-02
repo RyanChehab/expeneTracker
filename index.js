403242f994a31bccac3498f0290d395cc59bccb5
unknown <ryanchehab20@gmail.com>
2024-11-02 14:28:04 +0200

    added script

---
+++ index.js
@@ -0,0 +1,9 @@
+const income = document.getElementById('income')
+const expense = document.getElementById('expense')
+
+income.addEventListener('click', function(){
+    // create the income div
+    let income_sheet = document.createElement('div');
+    income_sheet.classList.add("income_sheet");
+    
+})
